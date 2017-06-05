30148aa3effae33452c9ae5e438ab3d431af9405
Geert van Horrik <geert@catenalogic.com>
2017-06-05 14:30:53 +0200

    Add footer to contribute to the docs

--- scripts - generate docs.bat
+++ scripts - generate docs.bat
@@ -5,8 +5,8 @@ REM )
 REM tools\docnet\docnet.exe src/4.5
 REM copy .\4.5\Home.htm .\4.5\index.htm
 
-tools\docnet\docnet.exe src/vNext
-copy .\vNext\Home.htm .\vNext\index.htm
+tools\docnet\docnet.exe src/vnext
+copy .\vNext\Home.htm .\vnext\index.htm
 
 tools\docnet\docnet.exe src/welcome
 copy .\welcome\Home.htm .\welcome\index.htm
