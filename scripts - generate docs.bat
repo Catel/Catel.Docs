43dabd4c1b825d0e9e8ea23d482f4fe4ecf591ab
Geert van Horrik <geert@catenalogic.com>
2017-07-05 14:36:47 +0200

    Regenerate docs with the latest version of DocNet

--- scripts - generate docs.bat
+++ scripts - generate docs.bat
@@ -7,6 +7,7 @@ REM copy .\4.5\Home.htm .\4.5\index.htm
 
 tools\docnet\docnet.exe src/vnext
 copy .\vNext\Home.htm .\vnext\index.htm
+copy .\vNext\404.htm .\404.htm
 
 tools\docnet\docnet.exe src/welcome
 copy .\welcome\Home.htm .\welcome\index.htm
