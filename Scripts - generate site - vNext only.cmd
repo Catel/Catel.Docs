213a6e34cd0039733782ca6e9c306476f17f97a8
Geert van Horrik <geert@catenalogic.com>
2017-09-10 15:13:52 +0200

    Decrease size of generated pages

---
+++ Scripts - generate site - vNext only.cmd
@@ -0,0 +1,25 @@
+:: Copy theme from subrepository
+xcopy "repositories\hugo-theme-docdock" "src\shared\themes\docdock" /e /y /i
+
+:: Generate all sites
+for %%i in (vnext) do (call:buildSite %%i)
+
+pause
+goto:eof
+
+
+:buildSite
+set siteName=%~1
+
+echo Building site: %siteName%
+
+:: Copy shared stuff
+xcopy "src\shared" "src\%siteName%" /e /y /i
+
+:: Build site
+cd .\src\%siteName%
+hugo
+
+:: Back to root 
+cd ..\..\
+goto:eof
